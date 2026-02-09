50e10fbfc4707c68f923fead0fe494e26d8f3db0
Shanmuga Ganesh <1999ganesh2@gmail.com>
2026-02-08 23:51:54 -0500

    fix: update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: './',
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.md'],
   server: {
